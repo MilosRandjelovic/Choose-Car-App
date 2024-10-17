6a09448e4de317681c868f9b3af36267dec1a8fc
MilosRandjelovic <mrandjelovic1979@gmail.com>
2024-10-17 13:56:04 +0200

    create header, hero,category,mostsearchedcar,infosection,footer components and add listinng cart

---
+++ src/add-listing/components/TextAreaField.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { Textarea } from "@/components/ui/textarea";
+
+function TextAreaField({ item, handleInputChange, carInfo }) {
+  return (
+    <div>
+      <Textarea
+        onChange={(e) => handleInputChange(item.name, e.target.value)}
+        required={item.required}
+        defaultValue={carInfo?.[item.name]}
+      />
+    </div>
+  );
+}
+
+export default TextAreaField;
